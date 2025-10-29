aa38656d1581f55981c040e8b38d4f7ac6984438
Pete Maiser <pete@maiser.com>
2025-10-29 14:52:27 -0500

    home-menu views (#13)
    * split MainMenuView out of HomeView / added HomeView 'back'
    * made SignUpInLink always visible as a trailing toolbar item
    * added MainViewPreviewWrapper

--- contacts.swiftdata/Views/Main/HomeView.swift
+++ contacts.swiftdata/Views/Main/HomeView.swift
@@ -2,7 +2,8 @@
 //  HomeView.swift
 //
 //  Template created by Pete Maiser, July 2024 through May 2025
-//      Template v0.1.1 Fast Five Products LLC's public AGPL template.
+//  Split from MenuView ~restored by Pete Maiser, Fast Five Products LLC, on 10/23/25.
+//      Template v0.2.3 (updated) Fast Five Products LLC's public AGPL template.
 //
 //  Copyright © 2025 Fast Five Products LLC. All rights reserved.
 //
@@ -18,181 +19,112 @@
 
 
 import SwiftUI
-import SwiftData
 
 struct HomeView: View {
     @ObservedObject var currentUserService: CurrentUserService
     @ObservedObject var announcementStore: AnnouncementStore
-    @ObservedObject var publicCommentStore: PublicCommentStore
-    @ObservedObject var privateMessageStore: PrivateMessageStore
-
-    @State private var showMenu = false
-    @State private var selectedMenuItem: NavigationItem? = .contacts
+    
+    @State private var heroBottomY: CGFloat = 0     // Tracks hero text bottom position
+    let tabBarSpace: CGFloat = 0                    // Leave space for Tab Bar (if applicable)
     
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading, spacing: 24) {
-                self.destinationView
-            }
-            .navigationTitle(selectedMenuItem?.label ?? "Home")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    self.menuView
-                }
+        GeometryReader { geo in
+            ZStack(alignment: .top) {
+                // MARK: Video Background
+                VideoBackgroundView()
+
+                // MARK: Hero Message
+                HeroView()
+                    .background(
+                        GeometryReader { proxy in
+                            Color.clear
+                                .onAppear {
+                                    updateHeroY(from: proxy, geo: geo)
+                                }
+                                .onChange(of: proxy.frame(in: .named("content")).maxY) { _, _ in
+                                    updateHeroY(from: proxy, geo: geo)
+                                }
+                        }
+                    )
                 
-                if !currentUserService.isSignedIn && self.selectedMenuItem != .profile {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        SignUpInLinkView(currentUserService: currentUserService, inToolbar: true)
+                // MARK: Announcements
+                if announcementStore.list.count > 0 {
+                    VStack {
+                        Spacer()
+                            .frame(height: heroBottomY + 16)
+                        
+                        VStackBox() {
+                            ViewThatFits(in: .vertical) {
+                                VStack(alignment: .leading, spacing: 8) {
+                                    StoreListView(store: announcementStore)
+                                    SignUpInLinkView(currentUserService: currentUserService)
+                                }
+                                ScrollView {
+                                    VStack(alignment: .leading, spacing: 8) {
+                                        StoreListView(store: announcementStore)
+                                        SignUpInLinkView(currentUserService: currentUserService)
+                                    }
+                                    .padding(.bottom, 8)
+                                }
+                            }
+                        }
+                        .frame(maxHeight: max(0, geo.size.height - heroBottomY - tabBarSpace))
+                        .padding(.horizontal)
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
                 }
             }
-            .padding(.vertical)
+            .coordinateSpace(name: "content")
         }
+        .padding(.vertical)
         .dynamicTypeSize(...ViewConfig.dynamicSizeMax)
         .environment(\.font, Font.body)
     }
-    
-    @ViewBuilder
-    var destinationView: some View {
-        switch self.selectedMenuItem {
-        case .announcements:
-            VStackBox {
-                StoreListView(store: announcementStore)
-                if !currentUserService.isSignedIn {
-                    SignUpInLinkView(currentUserService: currentUserService)
-                }
-            }
-            Spacer()
-            
-        case .contacts:
-            ContactListView(currentUserService: currentUserService)
-                        
-        case .profile:
-            UserAccountView(
-                viewModel: UserAccountViewModel(),
-                currentUserService: currentUserService)
-            
-        case .messages:
-            if currentUserService.isSignedIn {
-                UserMessagePostsStackView(
-                    viewModel: UserPostViewModel<PrivateMessage>(),
-                    currentUserService: currentUserService,
-                    store: privateMessageStore)
-            } else {
-                VStackBox {
-                    Text("Not Signed In!")
-                    SignUpInLinkView(currentUserService: currentUserService)
-                }
-                Spacer()
-            }
-            
-        case .comments:
-            if currentUserService.isSignedIn {
-                CommentPostsStackView(
-                    currentUserService: currentUserService,
-                    store: publicCommentStore)
-            } else {
-                VStackBox {
-                    Text("Not Signed In!")
-                    SignUpInLinkView(currentUserService: currentUserService)
-                }
-                Spacer()
-            }
-            
-        case .activity:
-            ActivityLogView()
-            
-        case .settings:
-            SettingsView()
-            
-        case .none:
-            EmptyView()
-        }
-        
-    }
 
-    @ViewBuilder
-    var menuView: some View {
-        Menu {
-            ForEach(NavigationItem.allCases
-                .filter { $0.sortOrder.0 == 0 }
-                .sorted(by: { $0.sortOrder.1 < $1.sortOrder.1 })) { item in
-                    Button {
-                        selectedMenuItem = item
-                    } label: {
-                        menuLabel(item)
-                    }
-            }
-            Divider()
-            ForEach(NavigationItem.allCases
-                .filter { $0.sortOrder.0 == 1 }
-                .sorted(by: { $0.sortOrder.1 < $1.sortOrder.1 })) { item in
-                    Button {
-                        selectedMenuItem = item
-                    } label: {
-                        menuLabel(item)
-                    }
-            }
-        } label: {
-            Label("Menu", systemImage: "line.3.horizontal")
-        }
-    }
-    
-    @ViewBuilder
-    func menuLabel(_ item: NavigationItem) -> some View {
-        if item == .profile {
-            Label(item.label, systemImage: currentUserService.isSignedIn ? "\(item.systemImage).fill" : item.systemImage)
+    private func updateHeroY(from proxy: GeometryProxy, geo: GeometryProxy) {
+        let y = proxy.frame(in: .named("content")).maxY
+        if y.isFinite && y > 0 {
+            heroBottomY = min(y, geo.size.height * 0.5)
         } else {
-            Label(item.label, systemImage: item.systemImage)
+            heroBottomY = geo.size.height * 0.5
         }
     }
 }
 
 
 #if DEBUG
-#Preview ("test-data signed-in") {
-    let currentUserService = CurrentUserTestService.sharedSignedIn
-    
-    let schema = Schema([
-        Contact.self,
-        ActivityLogEntry.self
-    ])
-    let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
-    let container = try! ModelContainer(for: schema, configurations: [config])
-
-    for object in ActivityLogEntry.testObjects {
-        container.mainContext.insert(object)
-    }
-    
-    for object in Contact.testObjects {
-        container.mainContext.insert(object)
-    }
-
-    let modelContainerManager = ModelContainerManager(currentUserService: currentUserService)
-    modelContainerManager.injectPreviewContainer(container)
-
-    return HomeView(
-        currentUserService: currentUserService,
-        announcementStore: AnnouncementStore.testLoaded(),
-        publicCommentStore: PublicCommentStore.testLoaded(),
-        privateMessageStore: PrivateMessageStore()              // loading empty because private messages not used yet
+#Preview ("View Only") {
+    let cuts = CurrentUserTestService.sharedSignedIn
+    HomeView(
+        currentUserService: cuts,
+        announcementStore: AnnouncementStore.testLoaded()
     )
-    .modelContainer(container)
 }
-#Preview ("no-data and signed-out") {
-    let currentUserService = CurrentUserTestService.sharedSignedOut
-    
-    let container = try! ModelContainer()
-    
-    let modelContainerManager = ModelContainerManager(currentUserService: currentUserService)
-    modelContainerManager.injectPreviewContainer(container)
-
-    return HomeView(
-        currentUserService: currentUserService,
-        announcementStore: AnnouncementStore.testLoaded(),
-        publicCommentStore: PublicCommentStore.testLoaded(),
-        privateMessageStore: PrivateMessageStore.testLoaded()
-    )
-    .modelContainer(container)
+#Preview ("test-data signed-in") {
+    let cuts = CurrentUserTestService.sharedSignedIn
+    MainViewPreviewWrapper(currentUserService: cuts) {
+        HomeView(
+            currentUserService: cuts,
+            announcementStore: AnnouncementStore.testLoaded()
+        )
+    }
+}
+#Preview ("tiny announcement signed-out") {
+    let cuts = CurrentUserTestService.sharedSignedOut
+    MainViewPreviewWrapper(currentUserService: cuts) {
+        HomeView(
+            currentUserService: cuts,
+            announcementStore: AnnouncementStore.testTiny()
+        )
+    }
+}
+#Preview ("no announcements") {
+    let cuts = CurrentUserTestService.sharedSignedIn
+    MainViewPreviewWrapper(currentUserService: cuts) {
+        HomeView(
+            currentUserService: cuts,
+            announcementStore: AnnouncementStore()
+        )
+    }
 }
 #endif
